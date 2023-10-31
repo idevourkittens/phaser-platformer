0d93ec2ccc610d5a7ebbeb33711417e68f3f023b
Attacus Brown <bbdxd123456@gmail.com>
2023-10-31 15:55:39 -0600

    n

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 import './style.css'
+import Phaser from 'phaser';
+
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super("main-scene");
+   }
+   preload() {
+    this.load.atlas("robot", "robot.png", "robot.json");
+  }
+   create() {
+    let height = this.scale.height;
+    let width = this.scale.width;
+    let plr = this.add.sprite(width/2,height/2,"robot")
+   }
+   update() {}
+}
+
+/**@type {Phaser.Types.Core.GameConfig} */
+const config = {
+  type: Phaser.WEBGL,
+  width: 400,
+  height: 400,
+  scene: [MainScene]
+};
+
+const game = new Phaser.Game(config);

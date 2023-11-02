f1ce68681a660fd01160e4b154c00a602b10e057
Attacus Brown <bbdxd123456@gmail.com>
2023-11-02 16:13:17 -0600

    pron

--- main.js
+++ main.js
@@ -1,27 +1,93 @@
-import './style.css'
-import Phaser from 'phaser';
+import "./style.css";
+import Phaser from "phaser";
+
+const PLAYER_ANIMS = {
+	idle: "idle",
+	walk: "walk",
+	run: "run",
+	jump: "jump",
+};
 
 class MainScene extends Phaser.Scene {
-  constructor() {
-    super("main-scene");
-   }
-   preload() {
-    this.load.atlas("robot", "robot.png", "robot.json");
-  }
-   create() {
-    let height = this.scale.height;
-    let width = this.scale.width;
-    let plr = this.add.sprite(width/2,height/2,"robot")
-   }
-   update() {}
+	constructor() {
+		super("main-scene");
+	}
+
+	preload() {
+		this.load.atlas("robot", "robot.png", "robot.json");
+	}
+
+	create() {
+		// const height = this.scale.height;
+		// const width = this.scale.width;
+
+		// object destructuring
+		const { height, width } = this.scale;
+
+		let player = this.physics.add.sprite(
+			width / 2,
+			height / 2,
+			"robot",
+			"character_robot_idle.png"
+		);
+
+    player.setCollideWorldBounds(true);
+    player.setBounce(0.5);
+
+		// single frame
+		player.anims.create({
+			key: PLAYER_ANIMS.idle,
+			frames: [{ key: "robot", frame: "character_robot_idle.png" }],
+		});
+
+		player.anims.create({
+			key: PLAYER_ANIMS.jump,
+			frames: [{ key: "robot", frame: "character_robot_jump.png" }],
+		});
+
+		// multiple frames
+		player.anims.create({
+			key: PLAYER_ANIMS.run,
+			frames: player.anims.generateFrameNames("robot", {
+				start: 0,
+				end: 2,
+				prefix: "character_robot_run",
+				suffix: ".png",
+			}),
+			frameRate: 10, // frames per second
+			repeat: -1, // infinite repeat
+		});
+
+		player.anims.create({
+			key: PLAYER_ANIMS.walk,
+			frames: player.anims.generateFrameNames("robot", {
+				start: 0,
+				end: 7,
+				prefix: "character_robot_walk",
+				suffix: ".png",
+			}),
+			frameRate: 10, // frames per second
+			repeat: -1, // infinite repeat
+		});
+
+		player.play(PLAYER_ANIMS.walk);
+	}
+
+	update() {}
 }
 
-/**@type {Phaser.Types.Core.GameConfig} */
+/** @type {Phaser.Types.Core.GameConfig} */
 const config = {
-  type: Phaser.WEBGL,
-  width: 400,
-  height: 400,
-  scene: [MainScene]
+	type: Phaser.WEBGL,
+	width: 400,
+	height: 400,
+	scene: [MainScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 1250 },
+    }
+  }
 };
 
 const game = new Phaser.Game(config);
